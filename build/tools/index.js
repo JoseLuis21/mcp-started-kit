9bbf6759ea552fbfbac25d5b43d1b97eeb16f050
jose <josephluihs@gmail.com>
2025-07-24 16:02:20 -0400

    refactor(tools): remove deprecated weather tools and related files

--- build/tools/index.js
+++
@@ -1,6 +0,0 @@
-import { registerAlertsTools } from "./alerts.js";
-import { registerForecastTools } from "./forecast.js";
-export function registerAllTools(server) {
-    registerAlertsTools(server);
-    registerForecastTools(server);
-}
